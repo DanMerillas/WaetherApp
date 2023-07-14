95aab155dc91c2ea470f2a3b09f0dbfd890c6fd8
Daniel Merillas <dmerillasg@minsait.com>
2023-07-15 01:35:20 +0200

    Primer componente de ciudad por defecto y fecha de hoy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import raining from './images/Shower.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Modificado
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article className="App-left">
+        <div className='App-weather'>
+          <img src={raining} alt='weather'/>
+        </div>
+        <div className='App-weather App-number'>
+          15º
+        </div>
+        <div className='App-weather App-status'>
+          Shower
+        </div>
+        <div className='App-weather'>
+          <div className='App-weather-date'>Today</div>
+          <div className='App-weather-date'>Fri, 5 Jun</div>
+        </div>
+        <div className='App-weather App-location'>
+          Alcalá de Henares
+        </div>
+      </article>
+      <article className="App-right">
+      </article>
     </div>
   );
 }
